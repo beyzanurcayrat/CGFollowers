34f17768ad330f83c4707244be88be59a7b4cf46
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-04 15:34:56 +0300

    First Commit

--- CGFollowers/SceneDelegate.swift
+++ CGFollowers/SceneDelegate.swift
@@ -12,8 +12,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     //test
-    //sikerler
-//asd
+    //FJSJDF
+    //asd
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
@@ -21,6 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.rootViewController = createTabbar()
         window?.makeKeyAndVisible()
+    
+        //test
         
     }
     
