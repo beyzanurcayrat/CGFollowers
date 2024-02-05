b213061f886182b62600295f6d78251ea0d1bbbf
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-05 13:01:56 +0300

    Create MVC model

---
+++ CGFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  CGFollowers
+//
+//  Created by beyzanur cayrat on 5.02.2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String 
+}
---
+++ CGFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  CGFollowers
+//
+//  Created by beyzanur cayrat on 5.02.2024.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
