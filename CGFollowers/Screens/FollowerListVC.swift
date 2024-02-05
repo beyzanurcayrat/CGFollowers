6f7beae27d0b5af323e5f8b686fa845834e08393
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-05 20:30:05 +0300

    Create UICollectionView

--- CGFollowers/Screens/FollowerListVC.swift
+++ CGFollowers/Screens/FollowerListVC.swift
@@ -10,16 +10,30 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
-
+    var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    func configureViewController () {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-
-        
+    }
+    
+    
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
-            
-            
             switch result {
             case .success(let followers):
                 print(followers)
@@ -27,12 +41,29 @@ class FollowerListVC: UIViewController {
             case .failure(let error):
                 self.presentCFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
             }
-            
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+        
+    }
+    
+    func createThreeColumnFlowlayout() -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avaliableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = avaliableWidth / 3
+        
+        let flowLayout           = UICollectionViewFlowLayout()
+        flowLayout.sectionInset  = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize      = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return UICollectionViewFlowLayout()
+        
     }
 }
