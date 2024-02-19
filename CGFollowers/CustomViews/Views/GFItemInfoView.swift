95d3fdadec9774e4519414419a0f81a269224716
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-19 16:18:05 +0300

    ItemInfoView

---
+++ CGFollowers/CustomViews/Views/GFItemInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  GFItemInfoView.swift
+//  CGFollowers
+//
+//  Created by beyzanur cayrat on 19.02.2024.
+//
+
+import UIKit
+
+class GFItemInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
