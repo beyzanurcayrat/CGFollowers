7cf6bec4ea2ff2aea6808636e51b336031f6701c
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-19 15:41:42 +0300

    UserInfoVC

--- CGFollowers/Screens/UserInfoVC.swift
+++ CGFollowers/Screens/UserInfoVC.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class UserInfoVC: UIViewController {
     
-    let headerView = UIView()
+    let headerView  = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+    var itemViews: [UIView] = []
     
     var username: String!
     
@@ -17,12 +20,21 @@ class UserInfoVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        layoutUI()
+        getUserInfo()
         
+    }
+        
+        
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
-        layoutUI()
-        
+    }
+    
+    func getUserInfo() {
         NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
             guard let self = self else { return }
             
@@ -38,19 +50,57 @@ class UserInfoVC: UIViewController {
                 self.presentCFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
-        layoutUI()
     }
+
     
     
     func layoutUI() {
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+        
+        itemViews = [headerView, itemViewOne, itemViewTwo]
+        
+
         view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(itemViewOne)
+        view.addSubview(itemViewTwo)
+        
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+            
+            NSLayoutConstraint.activate([
+                headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+                
+                itemViewOne.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemViewOne.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+                
+                itemViewTwo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemViewTwo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+            
+        }
+        
+        itemViewOne.backgroundColor = .systemPink
+        itemViewTwo.backgroundColor =  .systemBlue
+        
+        headerView.translatesAutoresizingMaskIntoConstraints  = false
+        itemViewOne.translatesAutoresizingMaskIntoConstraints = false
+        itemViewTwo.translatesAutoresizingMaskIntoConstraints = false
+        
+   
         
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+            
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: 140),
+            
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight)
+
         ])
     }
     
@@ -60,6 +110,8 @@ class UserInfoVC: UIViewController {
         childVC.view.frame = containerView.bounds
         childVC.didMove(toParent: self)
     }
+    
+    
 
     
     
