afb0a8176721d081ebaa38bd3d0afb3d4294ad40
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-19 15:07:39 +0300

    UserInfoHeaderVC

---
+++ CGFollowers/CustomViews/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  CGFollowers
+//
+//  Created by beyzanur cayrat on 19.02.2024.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure(){
+        textColor                 = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor        = 0.90
+        lineBreakMode             = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+
+}
---
+++ CGFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  CGFollowers
+//
+//  Created by beyzanur cayrat on 19.02.2024.
+//
+
+import Foundation
+
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
