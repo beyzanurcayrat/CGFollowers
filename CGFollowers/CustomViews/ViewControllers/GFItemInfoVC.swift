fcc56497480d2711d69f52c98d13bf2a4a9f28ad
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-19 16:35:20 +0300

    ItemInfoVC - Subclass

---
+++ CGFollowers/CustomViews/ViewControllers/GFItemInfoVC.swift
@@ -0,0 +1,65 @@
+//
+//  GFItemInfoVC.swift
+//  CGFollowers
+//
+//  Created by beyzanur cayrat on 19.02.2024.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+    
+    let stackView   = UIStackView()
+    let itemInfoViewOne = GFItemInfoView()
+    let itemInfoViewTwo = GFItemInfoView()
+    let actionButton    = GFButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layoutUI()
+        configureStackView()
+
+    }
+    
+    func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor   = .secondaryLabel
+    }
+    
+    private func configureStackView() {
+        stackView.axis           = .horizontal
+        stackView.distribution   = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+
+    }
+    
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44 )
+            
+            
+            
+        
+        ])
+    }
+    
+
+
+}
