72e892c67d98e91d7d9e7977e2899bb2e8e04b94
beyzanurcayrat <beyzacayrat@gmail.com>
2024-02-20 17:02:54 +0300

    Persistence Setup

--- CGFollowers/Screens/FollowerListVC.swift
+++ CGFollowers/Screens/FollowerListVC.swift
@@ -44,6 +44,9 @@ class FollowerListVC: UIViewController {
     func configureViewController () {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
     
     
@@ -109,6 +112,9 @@ class FollowerListVC: UIViewController {
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
     }
     
+    @objc func addButtonTapped() {
+        print("Add Button Tapped")
+    }
 }
 
 
